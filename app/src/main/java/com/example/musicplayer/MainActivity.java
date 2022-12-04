79ca7a1815becbad17ea9c01d546abcf3e4676a6
Nicolás <nico.alan.alb@gmail.com>
2022-12-04 00:26:16 -0300

    Audio added in res/raw folder. MediaPlayer instance creation and addition of activity_main.xml buttons listeners.

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -2,7 +2,9 @@ package com.example.musicplayer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MediaPlayer player = MediaPlayer.create(this, R.raw.fun_audio);
+        addPlayerButtonsListeners(player);
+    }
+
+    private void addPlayerButtonsListeners(MediaPlayer player) {
+        Button start = findViewById(R.id.start_button);
+        Button pause = findViewById(R.id.pause_button);
+
+        start.setOnClickListener(caughtEvent -> player.start());
+        pause.setOnClickListener(caughtEvent -> player.pause());
     }
 }
