b7f5b64a26fcee34046178fadf91d14f6fcc8882
Nicolás <nico.alan.alb@gmail.com>
2022-12-11 00:43:15 -0300

    Seekbar added to show music progress. It is done by running a runnable in UI thread and using a handler to post it to the thread Message queue.

--- app/src/main/java/com/example/musicplayer/MainActivity.java
+++ app/src/main/java/com/example/musicplayer/MainActivity.java
@@ -4,19 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.os.Handler;
 import android.widget.Button;
+import android.widget.SeekBar;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer player;
     private final MediaPlayer.OnCompletionListener listener = completion -> Toast.makeText(this, "I'm done", Toast.LENGTH_SHORT).show();
+    private final Handler hn = new Handler();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
     }
 
     @Override
@@ -27,27 +29,43 @@ public class MainActivity extends AppCompatActivity {
         player.setOnCompletionListener(listener);
     }
 
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (player != null) {
+            player.release();
+            player = null;
+        }
+    }
+
     private void addPlayerButtonsListeners() {
         Button start = findViewById(R.id.start_button);
         Button pause = findViewById(R.id.pause_button);
         Button addThreeSec = findViewById(R.id.add_three_sec_button);
         Button subThreeSec = findViewById(R.id.subtract_three_sec_button);
 
-        start.setOnClickListener(caughtEvent -> player.start());
+        start.setOnClickListener(caughtEvent -> {
+            player.start();
+            startSeekBar();
+        });
         pause.setOnClickListener(caughtEvent -> player.pause());
 
-        if(player != null) {
+        if (player != null) {
             addThreeSec.setOnClickListener(caughtEvent -> player.seekTo(player.getCurrentPosition() + 1000 * 3));
             subThreeSec.setOnClickListener(caughtEvent -> player.seekTo(player.getCurrentPosition() - 1000 * 3));
         }
     }
 
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if(player != null) {
-            player.release();
-            player = null;
-        }
+    private void startSeekBar() {
+        SeekBar sb = findViewById(R.id.music_seekbar);
+        sb.setMax(player.getDuration());
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                sb.setProgress(player.getCurrentPosition());
+                hn.postDelayed(this, 1);
+            }
+        });
     }
 }
